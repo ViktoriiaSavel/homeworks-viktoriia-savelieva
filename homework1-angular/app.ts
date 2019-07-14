479b66e3de7b4e2eabf25dd420824d42fd7b42df
viktoriia <vikmyronenko@gmail.com>
2019-07-14 23:03:16 +0300

    homework1-angular fixed review issues

--- homework1-angular/app.ts
+++ homework1-angular/app.ts
@@ -1,29 +1,41 @@
 //Определить переменные для всех примитивом джавасткрипта
-let a: number = 13;
-let b: string = 'text';
-let c: boolean = true;
-let d: null;
+let numberVar: number = 13;
+let stringVar: string = 'text';
+let booleanVar: boolean = true;
+let nullVar: null;
 
 //Написать функцию принимающую число и возвращающую его факториал по всем правилам тайпскрипта
 function getFactorial(num: number):number{
-    let factorial: number = 1;
+    if (num < 0) {
+        console.log("Please enter a number more than or equal to 0.");
+    } else {
+        let factorial: number = 1;
 
-    for(let i = num; i > 1; i--){
-        factorial *= i;
+        for(let i = num; i > 1; i--){
+            factorial *= i;
+        }
+        return factorial;
     }
-    return factorial;
 } 
 
-console.log(getFactorial(8));
+console.log(getFactorial(-2));
 
 //Написать функцию принимающую число и возврашающую массив с числами фибоначи до этого порядкового числа
 function getFibonacci(n:number):Array<number> {
-    let fibonacciArr: Array<number> = [0, 1];
+    if (n < 0) {
+        console.log("Please enter a number more than or equal to 0.");
+    } else {
+        if (n === 0 || n === 1) {
+            return [n];
+        } else {
+            let fibonacciArr: Array<number> = [0, 1];
 
-    for(let i = 2; i < n; i++) {
-        fibonacciArr[i] = fibonacciArr[i - 1] + fibonacciArr[i - 2];
+            for(let i = 2; i < n; i++) {
+            fibonacciArr[i] = fibonacciArr[i - 1] + fibonacciArr[i - 2];
+            }
+            return fibonacciArr;
+        }
     }
-    return fibonacciArr;
 }
 
-console.log(getFibonacci(7));
+console.log(getFibonacci(-2));
